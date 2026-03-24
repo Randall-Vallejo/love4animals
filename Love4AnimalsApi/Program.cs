ab394c474a85537e443d04658f9c40bf5124ebb4
Randall Vallejo <randall.vallejo@ucb.edu.bo>
2026-03-24 19:27:13 -0400

    Eliminacion de comentarios

--- Love4AnimalsApi/Program.cs
+++ Love4AnimalsApi/Program.cs
@@ -5,14 +5,12 @@ using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        options.JsonSerializerOptions.PropertyNamingPolicy = null; // Use PascalCase
+        options.JsonSerializerOptions.PropertyNamingPolicy = null; 
     });
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<IUserService, UserService>();
@@ -22,7 +20,6 @@ builder.Services.AddSingleton<ICampaignRepository, CampaignRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
